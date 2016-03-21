4212bcd45134b0a674865ff543cc50231d27252b
Callum Bryant <callum.l.bryant@gmail.com>
2016-03-21 12:05:01 +1100

    Update pagescrape.php

--- pagescrape.php
+++ pagescrape.php
@@ -4,10 +4,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Page Scrape</title>
-  <link rel="stylesheet" type="text/css" href="styles/main.css">
-  <meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Page Scrape</title>
+	<link rel="stylesheet" type="text/css" href="styles/main.css">
+	<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
