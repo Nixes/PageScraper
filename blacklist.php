f1e568aebb60b2afec6db2a3c316e64889d3b958
Nixes <callum@see-bry.com>
2016-07-04 13:54:32 +1000

    fix some syntax errors

--- blacklist.php
+++ blacklist.php
@@ -1,29 +1,21 @@
 <?php
 
-$junk_attribues = [
-  // these two remove most comment sections
-  array(regex => "/comment/i", attribute => "id"),
-  array(regex => "/comment/i", attribute => "class"),
-
-   // this is an ieee spectrum specific regex
-  array(regex => "/iso-content/i", attribute => "id")
-]
-
-$bad_tags = [
-  // asides very often contain js metadata
-  "aside",
-
-  // we don't really support lists, and they are often a very large source of irrelevant <p> tags
-  "ul",
-  "ol"
-]
-
-
 function containsJunk($childNode) {
+
+  $junk_attribues = [
+    // these two remove most comment sections
+    array(regex => "/comment/i", attribute => "id"),
+    array(regex => "/comment/i", attribute => "class"),
+
+     // this is an ieee spectrum specific regex
+    array(regex => "/iso-content/i", attribute => "id")
+  ];
+
+
   foreach($junk_attribues as $junk_attribute) {
     if ( preg_match($junk_attribute["regex"],$childNode->getAttribute($junk_attribute["attribute"])) ) {
       if (isset($GLOBALS["debug"]) && $GLOBALS["debug"]==1) {
-        echo "<p>Regex: ".$junk_attribute["regex"]." For Attribute: ".$junk_attribute["attribute"])."  Detected and Removed Element</p>";
+        echo "<p>Regex: ".$junk_attribute["regex"]." For Attribute: ".$junk_attribute["attribute"]."  Detected and Removed Element</p>";
       }
       return true;
     } else {
@@ -33,6 +25,17 @@ function containsJunk($childNode) {
 }
 
 function containsBadTag ($childNode) {
+  
+  $bad_tags = [
+    // asides very often contain js metadata
+    "aside",
+
+    // we don't really support lists, and they are often a very large source of irrelevant <p> tags
+    "ul",
+    "ol"
+  ];
+
+
   foreach($bad_tags as $bad_tag) {
     if ( $childNode->tagName == $bad_tag ) {
       return true;
