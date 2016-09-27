a66eec21e6c66b7837119adca344cfc32325c9ae
Nixes <callum.l.bryant@gmail.com>
2016-09-27 15:19:57 +1000

    fix syntax woops

--- tests.php
+++ tests.php
@@ -4,7 +4,7 @@ require 'pagescrape.php';
 use PHPUnit\Framework\TestCase;
 
 class TestPageScraper extends TestCase {
-  public function Test_findHighestIndex {
+  public function Test_findHighestIndex () {
     $test_array = array(0,2,5,7,9,10,50);
     $correct_result = 6;
 
