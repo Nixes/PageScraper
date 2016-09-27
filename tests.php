07f362cf24fc26a7217625f34edcc947bac19302
Nixes <callum.l.bryant@gmail.com>
2016-09-27 15:14:55 +1000

    first PHP unit test

---
+++ tests.php
@@ -0,0 +1,14 @@
+<?php
+require 'pagescrape.php';
+
+use PHPUnit\Framework\TestCase;
+
+class TestPageScraper extends TestCase {
+  public function Test_findHighestIndex {
+    $test_array = array(0,2,5,7,9,10,50);
+    $correct_result = 6;
+
+    $this->assertEquals($correct_result , findHighestIndex($test_array) );
+  }
+}
+?>
