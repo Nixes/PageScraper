c743303c99cd9fc26a50dd7bfe9818ecf4f1c50a
Nixes <callum@see-bry.com>
2016-10-02 18:51:27 +1100

    add test case for convertRelToAbs

--- tests/tests.php
+++ tests/tests.php
@@ -8,5 +8,17 @@ class TestPageScraper extends PHPUnit_Framework_TestCase {
 
     $this->assertEquals($correct_result , findHighestIndex($test_array) );
   }
+
+  public function test_convertRelToAbs () {
+    $GLOBALS["location"] = "http://www.someweb.com";
+    $test_url = "./something/image.jpg";
+
+    $correct_result = "http://www.someweb.com/something/image.jpg";
+
+    $result = convertRelToAbs($test_url);
+
+    echo "Final url was: ".$result."\n";
+    echo "Expected url was: ".$correct_result."\n";
+  }
 }
 ?>
