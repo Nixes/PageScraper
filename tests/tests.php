4981d8e5c0b5d00bace07a859eecde087a86445b
Nixes <callum@see-bry.com>
2017-10-14 13:44:49 +1100

    fix up tests

--- tests/tests.php
+++ tests/tests.php
@@ -2,25 +2,46 @@
 require 'lib.php';
 
 class TestPageScraper extends PHPUnit_Framework_TestCase {
+
+/**
+ * function that uses reflection to get allow executing private methods within an object for testing
+ * @param object $obj
+ * @param string $method_name
+ * @param array $args
+ */
+function callMethod($obj, $method_name, array $args) {
+  $class = new \ReflectionClass($obj);
+  $method = $class->getMethod($method_name);
+  $method->setAccessible(true);
+  return $method->invokeArgs($obj, $args);
+}
+
   public function test_findHighestIndex () {
     $test_array = array(0,2,5,7,9,10,50);
     $correct_result = 6;
 
-    $this->assertEquals($correct_result , findHighestIndex($test_array) );
+    $pagescraper = new Pagescraper;
+
+    // test private method
+    $result = callMethod($pagescraper,'findHighestIndex',array( $test_array ));
+
+    $this->assertEquals($correct_result ,$result );
   }
 
-  //public function test_convertRelToAbs () {
-    //$GLOBALS["location"] = "http://www.someweb.com";
-    //$test_url = "/something/image.jpg";
+  public function test_convertRelToAbs () {
+    $location = "http://www.someweb.com";
+    $test_url = "/something/image.jpg";
 
-    //$correct_result = "http://www.someweb.com/something/image.jpg";
+    $correct_result = "http://www.someweb.com/something/image.jpg";
 
-    //$result = convertRelToAbs($test_url);
 
-    //echo "Final url was: ".$result."\n";
-    //echo "Expected url was: ".$correct_result."\n";
-    //$this->assertEquals($correct_result , $result );
-  //}
+    $pagescraper = new Pagescraper;
+    $result = callMethod($pagescraper,'convertRelToAbs',array( $test_url,$location ));
+
+    echo "Final url was: ".$result."\n";
+    echo "Expected url was: ".$correct_result."\n";
+    $this->assertEquals($correct_result , $result );
+  }
 
   public function test_realArticles () {
     $urls =  array(
